541267ec010382e3d766d4a023f1601d97e6545c
adriftShivam <120205421+adriftShivam@users.noreply.github.com>
2026-04-26 14:19:29 +0530

    Add PUT users route

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,3 +36,23 @@ export const deleteUser = (req, res) => {
     user: deletedUser[0]
   });
 };
+
+
+
+export const updateUser = (req, res) => {
+  const id = Number(req.params.id);
+  const { name } = req.body;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  user.name = name || user.name;
+
+  res.json({
+    message: "User updated",
+    user
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUsers, createUser, deleteUser } from "../controllers/userController.js";
+import { getUsers, createUser, deleteUser, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/", createUser);
 router.delete("/:id", deleteUser);
+router.put("/:id", updateUser);
 
 export default router;
