f911f48f011851dafd600abf5f354ad078aeb73b
adriftShivam <120205421+adriftShivam@users.noreply.github.com>
2026-04-26 11:55:54 +0530

    Add GET and POST user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 import users from "../data/users.js";
 
+// GET all users
 export const getUsers = (req, res) => {
   res.json(users);
+};
+
+// POST new user
+export const createUser = (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name
+  };
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import { getUsers, createUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/", createUser);
 
 export default router;
