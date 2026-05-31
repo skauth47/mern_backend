01cdfe376c3418cb9aff5f94f8c814039025ec90
adriftShivam <120205421+adriftShivam@users.noreply.github.com>
2026-05-31 11:48:43 +0530

    Add API landing page

--- server.js
+++ server.js
@@ -10,17 +10,19 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/api/users", userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(5000, () => {
-      console.log("Server Running on Port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port ${PORT}`);
     });
   })
   .catch((err) => {
